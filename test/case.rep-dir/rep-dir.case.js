1fe2ad918a6aaf615206c49faf76d6e01057e13c
Fedor Golubev <fgolubev@yandex-team.ru>
2013-02-05 17:42:05 +0400

    rename option 'repDir' to 'gitDir'

--- test/case.rep-dir/rep-dir.case.js
+++ test/case.rep-dir/rep-dir.case.js
@@ -6,19 +6,19 @@ var localPath = tc.fixPath('.');
 abc.async.sequence(
     [
         function (callback) {
-            tc.callRep('init', localPath, {repDir: '.my2'}, callback);
+            tc.callRep('init', localPath, {gitDir: '.my2'}, callback);
         },
         function (callback) {
-            tc.callRep('getChanges', localPath, {repDir: '.my2'}, function (changes) {
+            tc.callRep('getChanges', localPath, {gitDir: '.my2'}, function (changes) {
                 tc.out(JSON.stringify(changes));
                 callback();
             });
         },
         function (callback) {
-            tc.callRep('commit', localPath, {repDir: '.my2'}, callback);
+            tc.callRep('commit', localPath, {gitDir: '.my2'}, callback);
         },
         function (callback) {
-            tc.callRep('getVersion', localPath, {repDir: '.my2'}, function (version) {
+            tc.callRep('getVersion', localPath, {gitDir: '.my2'}, function (version) {
                 tc.out(version.length);
                 callback();
             });
@@ -27,8 +27,9 @@ abc.async.sequence(
             tc.execConsole('rm -r ' + tc.fixPath('./.my2'), callback);
         }
     ], function () {
-    tc.finish();
-})
+        tc.finish();
+    }
+)
 
 //out
 {"modified":[],"added":["rep-dir.case.js","tc.conf.js"],"deleted":[]}
